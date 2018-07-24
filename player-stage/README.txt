39920f35e0ee7fb8dc12276fb1ea4299a10b17ec
Dennis Stampfer <stampfer@hs-ulm.de>
2018-07-24 11:31:33 +0200

    Add simulator data for player stage simulator

---
+++ player-stage/README.txt
@@ -0,0 +1,3 @@
+Please note:
+
+The files in this directory are based on the example environment that are provided by the official player/stage project. They have been adopted and extended by Service Robotics Ulm SRRC for the lecture Autonomous Mobile Systems.
---
+++ player-stage/start-player-stage-world.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+
+PLAYER_BIN="player"
+PLAYER_DIR="$SMART_ROOT_ACE/src/ComponentRepository/ComponentPlayerStageSimulator/player_stage"
+
+world=$1
+
+if [ "$world" = "" ]; then
+
+	echo -n "
+Please choose a world to load:
+
+(0) Hospital World
+
+(1) Racetrack World (SRRC Laboratory Scale)
+(2) Cylinder World (SRRC Laboratory Scale)
+(3) Corner World (SRRC Laboratory Scale)
+(4) Wallfollower World (SRRC Laboratory Scale)
+(5) Wallfollower World 2 (SRRC Laboratory Scale)
+
+(6) Simple World
+
+> "
+
+	read world
+
+fi
+
+case "$world" in
+        0)
+                playercfg="smart_hospital.cfg"
+                ;;
+
+        1)
+                playercfg="smart_racetrack.cfg"
+                ;;
+
+        2)
+                playercfg="smart_cylinder.cfg"
+                ;;
+
+        3)
+                playercfg="smart_cornermap.cfg"
+                ;;
+
+        4)
+                playercfg="smart_wallfollower.cfg"
+                ;;
+
+        5)
+                playercfg="smart_wallfollower2.cfg"
+                ;;
+
+        6)
+                playercfg="simple.cfg"
+                ;;
+
+        *)
+                echo "Unknown World: '$world'"
+                exit
+                ;;
+esac
+
+DIR=`pwd`
+
+echo
+echo "Starting world $playercfg ..."
+echo 
+
+cd $PLAYER_DIR
+xterm -title "Player Stage Simulator Terminal" -e "pwd; $PLAYER_BIN $playercfg || bash; sleep 2" &
+cd $DIR
