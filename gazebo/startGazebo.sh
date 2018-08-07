fcbbaae6a5cc76e919c0f11cb34f6e704a7aefae
Dennis Stampfer <stampfer@hs-ulm.de>
2018-08-07 14:28:55 +0200

    Updating gazebo simulator, removing repository-wide license file. See individual folders for licenses.

--- gazebo/startGazebo.sh
+++ gazebo/startGazebo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+FROM=$(pwd)
+cd $SMART_ROOT_ACE/repos/DataRepository/gazebo
+
 export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-8/
 export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:~/.gazebo/
 export GAZEBO_MODEL_PATH=:$SMART_ROOT_ACE/repos/DataRepository/gazebo/models
@@ -11,6 +14,9 @@ if [ $# -eq 0 ]
   then
     echo "No world file as argument supplied !"
   else
-    gazebo --verbose $1
+    xterm -title "Gazebo Terminal" -e "pwd; gazebo --verbose $1 || bash; sleep 5" &
+
+
 fi
 
+cd $FROM
